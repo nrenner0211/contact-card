4577c4d2070264957fa2172661fcaa2938553368
nrenner <nrenner0211@gmail.com>
2022-09-05 21:26:18 -0400

    webpack

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
+res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../client/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('../client/dist/'));
 
 app.use(express.static('../client/'));
 require('./routes/htmlRoutes.js')(app);
