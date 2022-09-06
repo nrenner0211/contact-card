a07b148badde5c4213d4c8e4ed9f929b5bc3c315
nrenner <nrenner0211@gmail.com>
2022-09-05 22:43:46 -0400

    indexeddb

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-
 module.exports = function(app) {
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
